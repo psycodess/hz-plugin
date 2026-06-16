c01d96ef11bab433f3c28c1302acd61b354f0718
psycodess <sifolatesla@gmail.com>
2026-06-16 07:09:32 +0600

    fix: correct instagram username in install.ps1

--- install.ps1
+++ install.ps1
@@ -41,4 +41,4 @@ Write-Host "=== Installation Successful! ===" -ForegroundColor Green
 Write-Host "Please restart Flow Launcher and trigger with 'hz'." -ForegroundColor Yellow
 
 # Open Instagram Profile
-Start-Process "https://instagram.com/pfychowhoqustionmark"
+Start-Process "https://instagram.com/psychowhoqustionmark"
